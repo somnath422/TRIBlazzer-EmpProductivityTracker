c11acb58f951210c4d565e0af2441829976e5966
Somnath Roy <somnath_roy@epam.com>
2024-12-06 10:37:23 +0530

    added mvc project

---
+++ EmployeeProductivityTracker/EmployeeProductivityTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeProductivityTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
